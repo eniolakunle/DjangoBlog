0899279f86ce50269ca00e05ab5a9b49cae4e1bc
Kunle Eniola <eniolakunle10@gmail.com>
2025-02-16 13:01:43 -0500

    media

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
