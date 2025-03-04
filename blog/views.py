1b36a467af5471814457e877a22a752049de395b
Kunle Eniola <eniolakunle10@gmail.com>
2025-03-04 16:59:56 -0500

    tags with links

--- blog/views.py
+++ blog/views.py
@@ -6,21 +6,28 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 # Create your views here.
+
 
 class PostListView(ListView):
 
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 7
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
-def post_list(request):
+
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+    paginator = Paginator(post_list, 7)
+    page_number = request.GET.get("page", 1)
 
     try:
         posts = paginator.page(page_number)
@@ -31,9 +38,13 @@ def post_list(request):
 
     return render(
         request,
-        'blog/post/list.html',
-        {'posts': posts}
+        "blog/post/list.html",
+        {
+            "posts": posts,
+            "tag": tag,
+        },
     )
+
 
 # def post_detail(request, id):
 #     try:
@@ -45,6 +56,7 @@ def post_list(request):
 #         'blog/post/detail.html',
 #         {'post': post}
 #     )
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -59,62 +71,56 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
 
     form = CommentForm()
-    
 
     return render(
         request,
-        'blog/post/detail.html',
+        "blog/post/detail.html",
         {
-            'post': post,
-            'form': form,
-            'comments': comments,
-            }
+            "post": post,
+            "form": form,
+            "comments": comments,
+        },
     )
 
+
 def post_share(request, post_id):
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
-    if request.method == 'POST':
+    if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url()
-            )
+            post_url = request.build_absolute_uri(post.get_absolute_url())
 
             subject = (
-                f"{cd['name']} ({cd['email']}) "
-                f"recommends you read {post.title}"
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
             )
-            message=(
+            message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
 
             send_mail(
                 subject=subject,
                 message=message,
                 from_email=None,
-                recipient_list=[cd['to']]
+                recipient_list=[cd["to"]],
             )
             sent = True
-        
+
     else:
         form = EmailPostForm()
 
     return render(
         request,
-        'blog/post/share.html',
+        "blog/post/share.html",
         {
-            'post': post,
-            'form': form,
-            'sent': sent,
-        }
-        )
+            "post": post,
+            "form": form,
+            "sent": sent,
+        },
+    )
+
 
 @require_POST
 def post_comment(request, post_id):
@@ -135,10 +141,10 @@ def post_comment(request, post_id):
 
     return render(
         request,
-        'blog/post/comment.html',
+        "blog/post/comment.html",
         {
-            'post': post,
-            'form': form,
-            'comment': comment,
-        }
+            "post": post,
+            "form": form,
+            "comment": comment,
+        },
     )
