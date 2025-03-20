d1ebbc573d5d7bec731dfede42bad449947c5ac4
Kunle Eniola <eniolakunle10@gmail.com>
2025-03-20 12:08:29 -0400

    css grid after breakpoint, and distinct posts for each suggestion

--- blog/views.py
+++ blog/views.py
@@ -81,7 +81,9 @@ def post_detail(request, year, month, day, post):
     # Similar posts
 
     post_tags_ids = post.tags.values_list("id", flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = (
+        Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id).distinct()
+    )
     # similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
     #     "-same_tags", "-publish"
     # )[:3]
