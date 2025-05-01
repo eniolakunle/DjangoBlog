bc58c8daf6158c779056aa31cdc24b0bea37f815
Kunle Eniola <eniolakunle10@gmail.com>
2025-05-01 11:05:48 -0400

    readme and share button text change

--- blog/static/js/scripts.js
+++ blog/static/js/scripts.js
@@ -62,7 +62,7 @@ function shareOnClick() {
       try {
         await navigator.share({
           title: document.title,
-          text: "You came to mind immediately, so I had to share this.",
+          text: "You came to mind immediately, I think you'll like this.",
           url: window.location.href,
         });
         console.log("Content shared successfully!");
