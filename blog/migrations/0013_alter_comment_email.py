43e624dbe1ad382a37a3644a4afd66f2bc2e86c5
eniolakunle <28786189+eniolakunle@users.noreply.github.com>
2025-05-07 23:14:10 +0000

    Ruff linting and formatting

--- blog/migrations/0013_alter_comment_email.py
+++ blog/migrations/0013_alter_comment_email.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('blog', '0012_alter_post_tags'),
+        ("blog", "0012_alter_post_tags"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='comment',
-            name='email',
+            model_name="comment",
+            name="email",
             field=models.EmailField(blank=True, max_length=254, null=True),
         ),
     ]
