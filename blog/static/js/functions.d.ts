5676f3750405a29588e5312a5aade43e1977e0d8
Kunle Eniola <eniolakunle10@gmail.com>
2025-08-29 18:27:17 -0400

    revert

--- blog/static/js/functions.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const intersectingObserver: IntersectionObserver;
-export declare function linkHandler(link: HTMLAnchorElement, overlay: Element): (e: Event) => void;
-export declare function fadeTransition(): void;
-export declare function endlessScrolling(): void;
-//# sourceMappingURL=functions.d.ts.map
