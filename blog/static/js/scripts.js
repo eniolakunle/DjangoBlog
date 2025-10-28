70bd8ddb88a516bcf29974219b38e547db15d96c
Kunle Eniola <eniolakunle10@gmail.com>
2025-10-27 21:13:36 -0400

    versioning

--- blog/static/js/scripts.js
+++ blog/static/js/scripts.js
@@ -1,6 +1,6 @@
 import { fadeTransition, endlessScrolling, searchLinks, callGemini
 //@ts-expect-error
- } from "./functions.js?v=1.0.3";
+ } from "./functions.js?v=1.0.4";
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.style.display =
--- blog/static/js/scripts.ts
+++ blog/static/js/scripts.ts
@@ -4,7 +4,7 @@ import {
   searchLinks,
   callGemini
   //@ts-expect-error
-} from "./functions.js?v=1.0.3";
+} from "./functions.js?v=1.0.4";
 
 
 
